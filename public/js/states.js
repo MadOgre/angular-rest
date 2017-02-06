015e876969c2f5b4d9d11bf0aadc1895ebb3ca66
Mike Semko <msemko@gmail.com>
2017-02-06 13:55:42 -0500

    add an article state to view a single article

--- public/js/states.js
+++ public/js/states.js
@@ -7,15 +7,33 @@
 		var homestate = {
 			name: "home",
 			url: "/",
-			component: "mainView"
+			component: "mainView",
+			resolve: {
+				articles: ["articlesService", function(articlesService) {
+					return articlesService.getAllArticles().then(function(data){
+						return data.data;
+					});
+				}]
+			}
 		};
 		var newarticle = {
 			name: "home.newarticle",
 			url: "newarticle",
 			component: "articleForm"
 		};
+		var article = {
+			name: "article",
+			url: "/article/{slug}",
+			component: "article",
+			resolve: {
+				article: ["articlesService", "$stateParams", function(articlesService, $stateParams) {
+					return articlesService.getArticle($stateParams.slug);
+				}]
+			}
+		};
 		$stateProvider.state(homestate);
 		$stateProvider.state(newarticle);
+		$stateProvider.state(article);
 		$locationProvider.html5Mode(true);
 	}]);
 })();
