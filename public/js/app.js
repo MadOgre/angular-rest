ac84788c9ee21f5a4f203ae33e03c57ec4a57b76
Mike Semko <msemko@gmail.com>
2017-02-06 13:56:57 -0500

    add an error handler in app.js to redirect to homepage if any transition error occurs

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,9 @@
 (function(){
 	"use strict";
-	angular.module("angular-rest", ["ui.router"]);
+	angular.module("angular-rest", ["ui.router"])
+	.run(function($transitions, $state) {
+		$transitions.onError({}, function(){
+			$state.go("home");
+		});
+	});
 })();
