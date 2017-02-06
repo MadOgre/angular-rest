29c4a7967971a9eae49271388fb5df09f5810be6
Mike Semko <msemko@gmail.com>
2017-02-06 13:56:17 -0500

    add link in mainview to link to a full article view by slug

--- public/js/components/mainview.js
+++ public/js/components/mainview.js
@@ -2,22 +2,21 @@
 	"use strict";
 	angular.module("angular-rest")
 	.component("mainView", {
-		template:
-			'<div>' +
-				'<a ui-sref="home.newarticle">Create New Article</a>' + 
-				'<p ng-repeat="article in $ctrl.articles track by $index">{{article.title}}</p>' +
-				'<ui-view cb="$ctrl.addArticle(article)"></ui-view>' +
-		    '</div>' +
-			'<h1>Welcome to homepage</h1>',
-		controller: ["articlesService", function(articlesService){
+		bindings: {
+			articles: "<"
+		},
+		controller: function(){
 			var vm = this;
-			articlesService.getAllArticles()
-			.then(function(data){
-				vm.articles = data.data;
-			});
 			vm.addArticle = function(article) {
 				vm.articles.push(article);
 			};
-		}]
+		},
+		template:
+			'<div>' +
+				'<a ui-sref="home.newarticle">Create New Article</a>' + 
+				'<p ng-repeat="article in $ctrl.articles track by $index"><a ui-sref="article({slug: article.slug})">{{article.title}}</a></p>' +
+				'<ui-view cb="$ctrl.addArticle(article)"></ui-view>' +
+		    '</div>' +
+			'<h1>Welcome to homepage</h1>'
 	});
 })();
