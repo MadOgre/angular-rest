adefe97b5729ba4d25cad81c073acea574a41f70
Mike Semko <msemko@gmail.com>
2017-02-07 16:40:57 -0500

    remove the now unnecessary controller, update state names

--- public/js/components/mainview.js
+++ public/js/components/mainview.js
@@ -5,17 +5,12 @@
 		bindings: {
 			articles: "<"
 		},
-		controller: function(){
-			var vm = this;
-			vm.addArticle = function(article) {
-				vm.articles.push(article);
-			};
-		},
+
 		template:
 			'<div>' +
-				'<a ui-sref="home.newarticle">Create New Article</a>' + 
-				'<p ng-repeat="article in $ctrl.articles track by $index"><a ui-sref="article({slug: article.slug})">{{article.title}}</a></p>' +
-				'<ui-view cb="$ctrl.addArticle(article)"></ui-view>' +
+				'<a ui-sref="home.mainview.newarticle">Create New Article</a>' + 
+				'<p ng-repeat="article in $ctrl.articles track by $index"><a ui-sref="home.article({slug: article.slug})">{{article.title}}</a></p>' +
+				'<ui-view></ui-view>' +
 		    '</div>' +
 			'<h1>Welcome to homepage</h1>'
 	});
